ed2439c5fdc45c7ac117ccf4d53ad1dd857b8890
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-07-15 09:31:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import sukkonimg from "./assets/images/Screenshot 2024-07-15 082301.png"
+import cross from "./assets/images/Screenshot 2024-07-15 082706.png"
+import MyHookForm from './forms/MyHookForm'
+
+function App() {
+  const [formData, setFormData] = useState({
+    fullname:"",
+    city:"",
+    mobilenumber:"",
+    dob:"",
+  })
+
+  return (
+    <div className='bg-black w-[100%] h-[100%] flex items-center justify-center '>
+        <div className='w-[300px] h-[550px]   relative rounded-md items-center p-6  bg-[#FEFFFE]'>
+
+          <div className='cross'>
+          <div className='right-2 absolute'>
+          <img
+          src={cross}
+          />
+        </div>
+          </div>
+           
+            <div className=' '>
+              <img
+              src={sukkonimg}
+              />
+             
+              
+              
+      
+            <div>
+              
+            </div>
+          </div>
+
+          <div>
+            <h1 className="font-bold uppercase mt-6 text-2xl">We would love </h1>
+            <h1 className='font-normal uppercase text-2xl '>To know about you!</h1>
+          </div>
+
+          <MyHookForm/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/forms/MyHookForm.jsx
@@ -0,0 +1,77 @@
+// src/components/MyHookForm.jsx
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const MyHookForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('Form data:', data);
+  };
+
+  return (
+    <form className='flex  flex-col gap-4 mt-6' onSubmit={handleSubmit(onSubmit)}>
+      <div className='flex flex-col gap-2  '>
+        <label className='text-[10px] pl-2 font-medium'>Full Name</label>
+        <input
+        required
+            className='border text-gray-400 border-gray-300 text-[10px] rounded-full w-full p-2'
+            placeholder='Enter your full name'
+          type="text"
+          {...register('fullName', { required: "Full name is required" })}
+        />
+        {errors.fullName && <span className="text-[8px] -mt-2 ml-2  text-yellow-500">{errors.fullName.text}</span>}
+      </div>
+
+      <div className='flex flex-col gap-1'>
+        <label className='text-[10px] pl-2 font-medium'>City:</label>
+        <input
+        required
+        placeholder='Enter your city name'
+        className='border text-gray-400 border-gray-300 text-[10px] rounded-full w-full p-2'
+          type="text"
+          {...register('city', { required: 'City is required' })}
+        />
+        {errors.city && <span className="text-[10px] -mt-1 ml-2  text-yellow-500" >{errors.city.message}</span>}
+      </div>
+
+      <div className='flex flex-col gap-1'>
+        <label className='text-[10px] pl-2 font-medium'>Mobile Number:</label>
+        <input
+        required
+        placeholder='Enter your mobile number'
+        className='border text-gray-400 border-gray-300 text-[10px] rounded-full w-full p-2'
+          type="text"
+          {...register('mobileNumber', {
+            required: {
+                value: true,
+                message: "Please enter your Phone Number.",
+              },
+              maxLength: { value: 12, message: "Invalid Phone Number" },
+              minLength: { value: 10, message: "Invalid Phone Number" },
+          })}
+        />
+        {errors.mobileNumber && <span className="text-[10px] -mt-1 ml-2  text-yellow-500">{errors.mobileNumber.message}</span>}
+      </div>
+
+      <div className='flex flex-col gap-1'>
+        <label className='text-[10px] pl-2 font-medium'>Date of Birth:</label>
+        <input
+        required
+        className='border text-gray-400 border-gray-300 text-[10px] rounded-full w-full p-2'
+          type="date"
+          {...register('dob', { required: 'Date of birth is required' })}
+        />
+        {errors.dob && <span className="text-[10px] -mt-1 ml-2  text-yellow-500">{errors.dob.message}</span>}
+      </div>
+
+      <button className='bg-[#FEC628] rounded-full py-2 text-[10px] mt-2 ' type="submit">Let the conversation begin</button>
+    </form>
+  );
+};
+
+export default MyHookForm;
